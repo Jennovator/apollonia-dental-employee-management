c23015f350bc30bf53de85765f9bd78043b4d759
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-15 15:39:55 -0500

    #Update: added API routes for employee and department

--- server/server.js
+++ server/server.js
@@ -34,3 +34,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to jenyll application." });
 });
+
+// routes for employee
+require("./app/routes/employee.routes")(app);
+
+// routes for department
+require("./app/routes/department.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
