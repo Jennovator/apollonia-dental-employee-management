2632d28c0b8061d929f0ca77a080de8d57f3b394
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-15 15:37:03 -0500

    #Add: added new routes for department and employee

---
+++ server/app/routes/employee.routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+    const employees = require("../controllers/employee.controller.js");
+
+    const router = require("express").Router();
+
+    // Create a new Employee
+    router.post("/", employees.create);
+
+    // Retrieve all Employees
+    router.get("/", employees.findAll);
+
+    // Retrieve Employee by Department
+    router.get("/:department", employees.findByDepartment);
+
+    // Retrieve a single Employee with id
+    router.get("/:id", employees.findOne);
+
+    // Update a Employee with id
+    router.put("/:id", employees.update);
+
+    // Delete a Employee with id
+    router.delete("/:id", employees.delete);
+
+    // Delete all Employees
+    router.delete("/", employees.deleteAll);
+
+    app.use('/api/employees', router);
+};
